6daa88bd2e1449bd6f6ef38a8a1e23c2eb35339f
Matt <matt@tankjetstudios.com>
2011-10-14 01:43:21 -0400

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prelaunch::Application.config.secret_token = '80e9498658e291f83356723338961769152454805fee33c87b4f2771d1524bdccd321fbcaf2d3015b5097f7305671dd9400337051a4c824a04e6fca0b61e07a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Prelaunch::Application.config.session_store :cookie_store, key: '_prelaunch_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Prelaunch::Application.config.session_store :active_record_store
